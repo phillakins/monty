92ef4d7c309db70559c70d57a9a9f2b2b0ab7b30
Philip Akinshipe <pakinshipe@gmail.com>
2023-01-19 10:03:43 +0000

    Updating file

--- stack_func.c
+++ stack_func.c
@@ -98,15 +98,3 @@ void free_dlistint(stack_t *head)
 	}
 }
 
-/**
- * f_stack - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_stack(stack_t **head, unsigned int counter)
-{
-	(void)head;
-	(void)counter;
-	bus.lifi = 0;
-}
